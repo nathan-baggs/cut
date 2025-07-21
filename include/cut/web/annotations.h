c32da7b1b12132542ffb2733e67e50d81e2e64ab
Nathan Baggs <iris-engine-dev@gmail.com>
2025-07-21 20:37:13 +0100

    Add Post annotation

--- include/cut/web/annotations.h
+++ include/cut/web/annotations.h
@@ -15,8 +15,14 @@ struct Get
     static constexpr auto method = "GET"sv;
 };
 
+struct Post
+{
+    static constexpr auto method = "POST"sv;
+};
+
 }
 
 static constexpr auto Get = annotations::Get{};
+static constexpr auto Post = annotations::Post{};
 
 }
