96771d6c0a35d8bb02fa07aa086f89193b8eb249
Nathan Baggs <iris-engine-dev@gmail.com>
2025-07-21 22:34:53 +0100

    Add to_json function

--- include/cut/utils/json.h
+++ include/cut/utils/json.h
@@ -1,12 +1,41 @@
 #pragma once
 
 #include <experimental/meta>
+#include <print>
+#include <string>
 #include <string_view>
 
 #include <nlohmann/json.hpp>
 
 namespace cut::utils
 {
+
+namespace details
+{
+
+template <class T>
+auto add_json_member(const T &obj, std::string_view key, ::nlohmann::json &json)
+{
+    if constexpr (^^T == ^^int)
+    {
+        json[key] = obj;
+    }
+    else if constexpr (^^T == std::meta::dealias(^^std::string))
+    {
+        json[key] = obj;
+    }
+    else
+    {
+        auto &next_key = json[key];
+
+        constexpr auto ctx = std::meta::access_context::current();
+        template for (constexpr auto member : std::define_static_array(std::meta::nonstatic_data_members_of(^^T, ctx)))
+        {
+            add_json_member(obj.[:member:], std::meta::display_string_of(member), next_key);
+        }
+    }
+}
+}
 
 template <class T>
 auto from_json(std::string_view json_str) -> T
@@ -25,6 +54,21 @@ auto from_json(std::string_view json_str) -> T
     }
 
     return res;
+}
+
+template <class T>
+auto to_json([[maybe_unused]] const T &obj) -> std::string
+{
+    auto json = ::nlohmann::json{};
+
+    constexpr auto ctx = std::meta::access_context::current();
+    template for ([[maybe_unused]] constexpr auto member :
+                  std::define_static_array(std::meta::nonstatic_data_members_of(^^T, ctx)))
+    {
+        details::add_json_member(obj.[:member:], std::meta::display_string_of(member), json);
+    }
+
+    return json.dump();
 }
 
 }
