f537050fd84054da312eb17a4fb84b1ebd618713
Nathan Baggs <iris-engine-dev@gmail.com>
2025-07-22 21:33:42 +0100

    Update samples

--- samples/main.cpp
+++ samples/main.cpp
@@ -3,7 +3,9 @@
 #include <string_view>
 
 #include "cut/coro/task.h"
+#include "cut/db/annotations.h"
 #include "cut/db/db.h"
+#include "cut/db/db_runner.h"
 #include "cut/web/annotations.h"
 #include "cut/web/app.h"
 #include "cut/web/controller_base.h"
@@ -24,11 +26,25 @@ struct Record
 };
 
 // clang-format off
+struct ProductModel
+{
+    [[=cut::db::Id]]
+    std::uint32_t id = 0;
+    std::string name;
+    std::uint32_t stock_count;
+};
+
+struct ProductCreateRequest
+{
+    std::string name;
+};
+// clang-format on
+
+// clang-format off
 class Foo : public cut::web::ControllerBase
 {
   public:
-    Foo(cut::db::Db *db) : db_(db){}
-
+    Foo() { }
     ~Foo() override = default;
 
     [[=cut::web::Get]]
@@ -51,18 +67,13 @@ class Foo : public cut::web::ControllerBase
     auto record(Person person) -> cut::coro::Task<cut::web::Response>
     {
         std::println("Foo record called");
-
-        const auto id = co_await db_->test();
-
-        co_return cut::web::Ok(Record{.person = person, .id = id});
+        co_return cut::web::Ok(Record{.person = person, .id = 1});
     }
 
     auto another_method() -> void
     {
     }
     
-private:
-    cut::db::Db *db_;
 };
 
 class AnotherController : public cut::web::ControllerBase
@@ -79,13 +90,38 @@ class AnotherController : public cut::web::ControllerBase
         co_return cut::web::Ok();
     }
 };
+
+class Product : public cut::web::ControllerBase
+{
+  public:
+    Product(cut::db::Db *db)
+        : db_(db) {};
+    ~Product() override = default;
+
+    [[=cut::web::Post]]
+    auto add(ProductCreateRequest product_create_request) -> cut::coro::Task<cut::web::Response>
+    {
+        cut::utils::log::info("Product::add called");
+
+        auto db_runner = cut::db::DbRunner{db_};
+        co_await db_runner.insert(ProductModel{
+            .name = product_create_request.name,
+            .stock_count = 0
+        });
+
+        co_return cut::web::Created();
+    }
+
+  private:
+    cut::db::Db *db_;
+};
 // clang-format on
 
 auto main() -> int
 {
     try
     {
-        auto app = cut::web::App<Foo, AnotherController>{};
+        auto app = cut::web::App<Foo, AnotherController, Product>{};
 
         app.run();
     }
