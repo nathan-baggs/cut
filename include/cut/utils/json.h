fc067c0d2f748b7ccea73c76852b913e0e14228d
Nathan Baggs <iris-engine-dev@gmail.com>
2025-07-22 19:30:00 +0100

    Handle arrays and objects in json parsing

--- include/cut/utils/json.h
+++ include/cut/utils/json.h
@@ -1,40 +1,90 @@
 #pragma once
 
+#include <cinttypes>
 #include <experimental/meta>
+#include <map>
 #include <print>
+#include <ranges>
 #include <string>
 #include <string_view>
+#include <vector>
 
 #include <nlohmann/json.hpp>
 
 namespace cut::utils
 {
 
+namespace annotations
+{
+struct IncludeMemberName
+{
+};
+}
+
+static constexpr auto IncludeMemberName = annotations::IncludeMemberName{};
+
 namespace details
 {
 
 template <class T>
-auto add_json_member(const T &obj, std::string_view key, ::nlohmann::json &json)
+struct JsonMember
 {
-    if constexpr (^^T == ^^int)
+    template <class U>
+    static constexpr auto add(const T &obj, U key, ::nlohmann::json &json, bool = false) -> void
     {
-        json[key] = obj;
-    }
-    else if constexpr (^^T == std::meta::dealias(^^std::string))
-    {
-        json[key] = obj;
-    }
-    else
-    {
-        auto &next_key = json[key];
-
-        constexpr auto ctx = std::meta::access_context::current();
-        template for (constexpr auto member : std::define_static_array(std::meta::nonstatic_data_members_of(^^T, ctx)))
+        if constexpr (^^T == ^^int)
         {
-            add_json_member(obj.[:member:], std::meta::display_string_of(member), next_key);
+            json[key] = obj;
+        }
+        else if constexpr (^^T == std::meta::dealias(^^std::string))
+        {
+            json[key] = obj;
+        }
+        else
+        {
+            auto &next_key = json[key];
+
+            constexpr auto ctx = std::meta::access_context::current();
+            template for (constexpr auto member :
+                          std::define_static_array(std::meta::nonstatic_data_members_of(^^T, ctx)))
+            {
+                JsonMember<typename[:std::meta::type_of(member):]>::add(
+                    obj.[:member:], std::meta::display_string_of(member), next_key);
+            }
         }
     }
-}
+};
+
+template <class T>
+struct JsonMember<std::vector<T>>
+{
+    static constexpr auto add(const std::vector<T> &obj, std::string_view key, ::nlohmann::json &json, bool = false)
+        -> void
+    {
+        auto &next_key = json[key];
+        for (auto i = 0u; i < obj.size(); ++i)
+        {
+            JsonMember<T>::add(obj[i], i, next_key);
+        }
+    }
+};
+
+template <class K, class V>
+struct JsonMember<std::map<K, V>>
+{
+    static constexpr auto add(
+        const std::map<K, V> &obj,
+        std::string_view key,
+        ::nlohmann::json &json,
+        bool include_member_name = false) -> void
+    {
+        for (const auto &[k, v] : obj)
+        {
+            std::println("k: {} {}", k, include_member_name);
+            JsonMember<V>::add(v, k, include_member_name ? json[key] : json);
+        }
+    }
+};
 }
 
 template <class T>
@@ -57,7 +107,7 @@ auto from_json(std::string_view json_str) -> T
 }
 
 template <class T>
-auto to_json([[maybe_unused]] const T &obj) -> std::string
+auto to_json(const T &obj) -> std::string
 {
     auto json = ::nlohmann::json{};
 
@@ -65,10 +115,12 @@ auto to_json([[maybe_unused]] const T &obj) -> std::string
     template for ([[maybe_unused]] constexpr auto member :
                   std::define_static_array(std::meta::nonstatic_data_members_of(^^T, ctx)))
     {
-        details::add_json_member(obj.[:member:], std::meta::display_string_of(member), json);
+        constexpr auto annotations = std::define_static_array(std::meta::annotations_of(member));
+        constexpr auto include_member_name = std::ranges::size(annotations) == 1;
+        details::JsonMember<typename[:std::meta::type_of(member):]>::add(
+            obj.[:member:], std::meta::display_string_of(member), json, include_member_name);
     }
 
     return json.dump();
 }
-
 }
